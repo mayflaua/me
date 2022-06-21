048443f900c0d1922812a1cee0d6e8588d7c1547
sweetie <num1carbon@gmail.com>
2022-06-21 22:35:48 +0300

    migrating on vue3

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV ==='production'
+  ? '/me/'
+  : '/',
   productionSourceMap: false,
   filenameHashing: false,
 
